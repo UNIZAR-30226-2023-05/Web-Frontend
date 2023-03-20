016d19370a22e998608b1d91404353f9ae9e2d5b
marinalamiel <marinalamielvi@gmail.com>
2023-03-20 12:19:33 +0100

    inicio

--- frontend/src/pages/inicio/inicio.js
+++ frontend/src/pages/inicio/inicio.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './inicio.css';
+import oca from '../../assets/img/oca.PNG'
 
 function Inicio() {
   return (
@@ -10,7 +11,16 @@ function Inicio() {
       <div className="body">
         Carrera hacia la meta
       </div>
+      <div className="img">
+        <img src={oca}/>
+      </div>
+      <button to=/log.js className="button">
+        JUEGA AHORA
+      </button>
     </div>
+    
+
+    
   );
 }
 
