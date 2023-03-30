896dcedeb481eede921a7a0deda15ffb951b15f4
Carlota <karlotakintana@gmail.com>
2023-03-30 19:52:50 +0200

    MODAL

--- frontend/src/pages/inicio/inicio.js
+++ frontend/src/pages/inicio/inicio.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import './inicio.css';
 import '../../components/Popup.css'
-import oca from '../../assets/img/oca.PNG'
 import { Link } from 'wouter';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root'); // para asegurarnos de que react-modal funcione correctamente
 
 function Inicio() {
-  const [modalIsOpen, setModalIsOpen] = useState(false); // estado para controlar si el popup está abierto o cerrado
+  const [logInmodalIsOpen, setlogInModalIsOpen] = useState(false); // estado para controlar si el popup está abierto o cerrado
 
   return (
     <>
@@ -20,18 +19,22 @@ function Inicio() {
         Carrera hacia la meta
       </div>
       
-      <button className="button" onClick={() => setModalIsOpen(true)}>
+      <button className="button" onClick={() => setlogInModalIsOpen(true)}>
         JUEGA AHORA
       </button>
       
-      <Modal className="popup" isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+      <Modal className="popup" isOpen={logInmodalIsOpen} onRequestClose={() => setlogInModalIsOpen(false)}>
         <div className="popup-content">
           <h2 className='titulo'>INICIAR SESIÓN</h2>
           <p className='texto'>E-mail</p>
           <input className='barraEscribir' type="text" placeholder="E-mail"/>
           <p className='texto'>Constraseña</p>
           <input className='barraEscribir' type="text" placeholder="Constraseña"/>
-          <button className='closeButton' onClick={() => setModalIsOpen(false)}>X</button>
+          <button className='closeButton' onClick={() => setlogInModalIsOpen(false)}>X</button>
+          <button className='startButton'>INICIAR SESIÓN</button>
+          <p className='texto'>¿No tienes cuenta?</p>
+          <p className='textoEspecifico'onClick={() => setlogInModalIsOpen(false)} >Regístrate</p>
+          
         </div>
       </Modal>
       
