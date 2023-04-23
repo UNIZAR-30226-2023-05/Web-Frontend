af8f614539c0e58c5150de7594eb4b913eb13663
marinalamiel <marinalamielvi@gmail.com>
2023-04-23 18:22:09 +0200

    juego

--- frontend/src/pages/juego/juego.js
+++ frontend/src/pages/juego/juego.js
@@ -3,7 +3,6 @@ import './juego.css';
 import { Link, useLocation } from 'wouter';
 import Modal from 'react-modal';
 import image from '../../assets/img/Skin_dorada.png'
-
 Modal.setAppElement('#root'); // para asegurarnos de que react-modal funcione correctamente
 
 function Juego() {
@@ -11,7 +10,7 @@ function Juego() {
   const [myTop, setTop] = useState("90%");
   const [myLeft, setLeft] = useState("30%");
   const [idFicha1, setIdCasilla] = useState(0);
-
+  
   
   /*Funcion para mover las fichas*/
   /*Fucnion para hacer la logica del juego: puentes, preguntas...*/
@@ -24,35 +23,29 @@ function Juego() {
     }
   }, [idFicha1])
 
-  
-  
+
+    
   return (
     <> 
-        <div className="imagenes">
-          <a href="/">
-            <img src={chat} alt="Chat" />
-          </a>
-          <a href="/">
-            <img src={amigos} alt="Amigos" />
-          </a>
-          <a href="/">
-            <img src={tienda} alt="Tienda" />
-          </a>
-          <a href="/">
-            <img src={ajustes} alt="Ajustes" />
-          </a>
-        </div>
-        <div className='contenedor'>  
-            <div className='tablero'>
-              <div className='ficha' style={ {top:myTop,left:myLeft}}>
-                <img className='fichaTam' src={image} />
-              </div> 
-            </div>
-            <div className='chat'>
-                <button onClick={() => setIdCasilla(1)}>saltar1</button>
-                <button onClick={() => setIdCasilla(2)}>saltar2</button>
-            </div>
-        </div> 
+      <header className="header">
+        JUEGO
+      </header>
+      <div className='cont'>
+        <button classname='botonAbandonar' as={Link} to='/principal'>Abandonar la sala</button>
+        <div className='barraTitulo'></div>
+      </div>
+      
+      <div className='contenedor'>  
+          <div className='tablero'>
+            <div className='ficha' style={ {top:myTop,left:myLeft}}>
+              <img className='fichaTam' src={image} />
+            </div> 
+          </div>
+          <div className='chat'>
+              <button onClick={() => setIdCasilla(1)}>saltar1</button>
+              <button onClick={() => setIdCasilla(2)}>saltar2</button>
+          </div>
+      </div> 
     </>
   );
 }
