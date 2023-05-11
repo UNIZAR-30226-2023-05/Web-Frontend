c8f506560fdbdc86e6a1280f61b31e3ec18397b0
Leonor Murphy <76400289+leonormurphy@users.noreply.github.com>
2023-05-11 12:01:35 +0200

    Estadísticas son null, no muestra nada

--- frontend/src/pages/stats/stats.js
+++ frontend/src/pages/stats/stats.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./stats.css";
 import "../../components/RestoPantallas.css";
 import Card from "../../components/Card";
@@ -6,6 +6,8 @@ import GetStats from "../../services/getStats.js";
 import GetID from "../../services/getID_log.js";
 
 function Stats() {
+  const [data, setData] = useState([]);
+
   const obtenerEstadisticas = async () => {
     // Info del usuario
     const email = localStorage.getItem("email");
@@ -14,24 +16,34 @@ function Stats() {
     let dataId = await GetID(email);
 
     // Coger los datos
-    let data = GetStats(dataId.id_usuario);
+    let data = await GetStats(dataId.id_usuario);
+
+    setData(data);
   };
 
-  const data = [
-    { title: "Card 1", data: "Data for card 1" },
-    { title: "Card 2", data: "Data for card 2" },
-    { title: "Card 3", data: "Data for card 3" },
-    { title: "Card 4", data: "Data for card 4" },
-    { title: "Card 5", data: "Data for card 5" },
-    { title: "Card 6", data: "Data for card 6" },
+  useEffect(() => {
+    obtenerEstadisticas();
+  }, []);
+
+  const prueba = [
+    { title: "Card 1", estadisticas: "Data for card 1" },
+    { title: "Card 2", estadisticas: "Data for card 2" },
+    { title: "Card 3", estadisticas: "Data for card 3" },
+    { title: "Card 4", estadisticas: "Data for card 4" },
+    { title: "Card 5", estadisticas: "Data for card 5" },
+    { title: "Card 6", estadisticas: "Data for card 6" },
   ];
 
   return (
     <div className="align-items-center">
       <header className="header">Estadísticas</header>
-      <div className="card-container" onClick={obtenerEstadisticas}>
-        {data.map((item) => (
-          <Card key={item.title} title={item.title} data={item.data} />
+      <div className="card-container">
+        {prueba.map((item) => (
+          <Card
+            key={item.title}
+            title={item.title}
+            data={item.estadisticas ? item.estadisticas : 0}
+          />
         ))}
       </div>
     </div>
